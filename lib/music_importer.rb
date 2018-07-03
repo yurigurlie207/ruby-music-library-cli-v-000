c7f5a71b760f908ae5c0ef6bca56a5f1dc05a4b6
Adrienne Yao <adrienne.yao@icloud.com>
2018-07-03 23:31:33 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -8,11 +8,12 @@ class MusicImporter
   end
 
   def files
-    @files = Dir.foreach(@path).select { |x| File.file?("#{@path}/#{x}") }
+    # @files =
+    Dir.foreach(@path).select { |x| File.file?("#{@path}/#{x}") }
   end
 
   def import
-    files
+    @files = files
     @files ? @files.each { |filename| Song.create_from_filename(filename) } : ""
   end
 
